ed236470e000fc2f5da01d79603b0b58a914aad8
Gokulnath16 <24774169+Gokulnath16@users.noreply.github.com>
2021-05-27 10:57:00 +0530

    first

---
+++ AllVowels/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AllVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "acebisouzz";
+            string a = "aeiou";
+            char[] c = s.ToCharArray();
+            StringBuilder sb = new StringBuilder();
+            foreach (char item in c)
+            {
+                if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u')
+                sb.Append(item);
+            }
+            string b = sb.ToString();
+            if (a == b)
+                Console.WriteLine("1");
+            else
+                Console.WriteLine("-1");
+            
+        }
+    }
+}
