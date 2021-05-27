cd5f1b03becd2b9fe063a9db915a7886379b686a
Gokulnath16 <24774169+Gokulnath16@users.noreply.github.com>
2021-05-27 11:58:06 +0530

    first

---
+++ CountVowel/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tripprob4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                UserProgramCode p = new UserProgramCode();
+                Console.WriteLine(" Enter string to count Vowels");
+                string st = Console.ReadLine();
+                int ans = p.countVowels(st);
+                Console.WriteLine(ans);
+                Console.ReadKey();
+            } while (true);
+        }
+    }
+}
+class UserProgramCode
+{
+    public int countVowels(string st)
+    {
+        st.ToLower();
+        int count = 0;
+        if (!st.All(char.IsLetter))
+        {
+            //Console.WriteLine("Input contains digit/special Symbols also");
+            count = -1;
+            goto finish;
+        }
+        char[] arr = st.ToCharArray();
+        foreach (var c in arr)
+        {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            {
+                count++;
+            }
+        }
+    finish:
+        return count;
+    }
+}
